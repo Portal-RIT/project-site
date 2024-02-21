2da0cfe7b60e7d934cf50f06a27ce5eee7dcd0f7
Nathan <nm7090@rit.edu>
2024-02-21 10:46:51 -0500

    started tracking page and NavBar Components

---
+++ src/Tracking.js
@@ -0,0 +1,28 @@
+
+
+function Tracking() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+function NavBar() {
+    return(
+        <div class="NavFlex">
+            <div>
+                <a>Home</a>
+            </div>
+            <div>
+                <a>Team</a>
+            </div>
+            <div>
+                <a>Features</a>
+            </div>
+            <div>
+                <a>Sprint</a>
+            </div>
+        </div>
+    )
+}
