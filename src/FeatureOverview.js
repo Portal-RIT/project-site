2ee65c36a7d3ca22f24916027c7b5c504eef354e
Nathan <nm7090@rit.edu>
2024-02-29 16:27:21 -0500

    added function to grab reqeusted feature to be displayed

--- src/FeatureOverview.js
+++ src/FeatureOverview.js
@@ -1,9 +1,22 @@
+import featureData from 'src/features.json';
 
+function grabFeature(featureName){
+    for (item in featureData) {
+        if(item.name == featureName) {
+            return item;
+        }
+    }
+}
 
-function FeatureOverview() {
+function FeatureOverview(props) {
+
+    feature = grabFeature(props.name)
+
     return(
         <div>
             <h1>Description</h1>
+            <br/>
+            <p>feature.description</p>
         </div>
     );
 }
