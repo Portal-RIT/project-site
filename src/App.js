13682c4108202d9223e78833d5fa82a4b285010d
Zoe <zmb6893@g.rit.edu>
2024-02-29 16:35:09 -0500

    Separated the sprints and features into a separate objects folder

--- src/App.js
+++ src/App.js
@@ -8,35 +8,8 @@ import Sprints from './pages/Sprints';
 import Feature from './pages/Feature';
 import Sprint from './pages/Sprint';
 import { BASE_LOCATION } from './routes';
-
-// Zoe's note: Make sure that each feature object always has a name in order for the navbar to work
-const features = [
-	{
-	  name: "Feature 1",
-	  start: "01-01-2024",
-	  end: "01-14-2024"
-	},
-	{
-	  name: "Feature 2",
-	  start: "01-14-2024",
-	  end: "01-28-2024"
-	},
-	{
-	  name: "Feature 3",
-	  start: "01-28-2024",
-	  end: "02-11-2024"
-	},
-]
-  
-// Zoe's note: Each sprint object in this array must have a name field to be used in the navbar
-const sprints = [
-	{
-	  name: "Sprint 1",
-	  start: "01-01-2024",
-	  end: "01-14-2024"
-	}
-]
-
+import { sprints } from './objects/sprints';
+import { features } from './objects/features';
 
 function App() {
 
---
+++ src/objects/sprints.js
@@ -0,0 +1,14 @@
+// Zoe's note: Each sprint object in this array must have a name field to be used in the navbar
+// More fields will definitely need to be added to this!
+export const sprints = [
+	{
+	  name: "Sprint 1",
+	  start: "01-01-2024",
+	  end: "01-14-2024"
+	},
+	{
+		name: "Sprint 2",
+		start: "01-14-2024",
+		end: "01-28-2024"
+	}
+]
