6a25099f90baa33ac67600fd54e7bf5075519ccd
Nathan <nm7090@rit.edu>
2024-03-01 08:42:44 -0500

    using features json file

--- src/FeatureOverview.js
+++ src/FeatureOverview.js
@@ -1,16 +1,27 @@
-import featureData from 'src/features.json';
+import { sprints } from './objects/sprints';
+import { features } from './objects/features';
 
 function grabFeature(featureName){
-    for (item in featureData) {
+    for(item in features) {
         if(item.name == featureName) {
             return item;
         }
     }
+
+    return false;
 }
 
 function FeatureOverview(props) {
 
     feature = grabFeature(props.name)
+
+    if(!(feature)) {
+        return (
+            <div>
+                <h1>Can Not Find This Feature</h1>
+            </div>
+        );
+    }
 
     return(
         <div>
