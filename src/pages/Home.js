d37bacda628002b593662e1249baf0df70415013
eas7048rit <eas7048@g.rit.edu>
2024-03-04 17:05:20 -0500

    Replaced lorum ipsum text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,22 @@ function Home() {
 	return (
 		<div style={{display: 'flex', flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
 			<div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '2rem'}}>
-				<div className="home_title_block" style={{}}>
-					<h1 style={{paddingTop: '1rem', color: '#0066B4'}}>Project Name</h1>
-					<h2>Team Name</h2>
+				<div className="home_title_block">
+					<h1 style={{paddingTop: '1rem', color: '#0066B4'}}>Novena Project Management Portal</h1>
+					<h2>Team: Portal</h2>
 				</div>
 
 				<div className='home_text_block'>
 					<h2 style={{ paddingLeft:'3%'}}>Project Summary</h2>
 					<p className='home_text'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+					Novena is an all in one portal where both customers and employees can interact with each other about a project. This portal will contain the project itself where it will have all parties be able to view and manage the project’s lifetime as well as receive notifications about the project. The project, specifically, will contain key actions from customers and JADAK employees, milestones, status of the project, updates of deployed projects, and a graphical timeline of the project. The portal will also provide chat functionality for both customer and employee to send chat messages as well as view emails sent from outside the portal. The portal will also contain a location to store already preprocessed training image datasets from a variety of options ready for download to be used for custom solutions on JADAK’s smart camera training. Employees will be able to upload training image datasets that can be preprocessed from a library of options they desire. Finally, there will be a place for customers and employees to view and access documents and licenses that are relevant to them.
 					</p>
 				</div>
 
 				<div className='home_text_block'>
 					<h2 style={{ paddingLeft:'3%'}}>Current Features</h2>
 					<p className='home_text'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+						No current features yet.
 					</p>
 				</div>
 			</div>
