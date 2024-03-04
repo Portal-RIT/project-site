b95ce31868c23d7376c36b315033ad93153915b6
eas7048rit <eas7048@g.rit.edu>
2024-03-04 13:19:27 -0500

    Created base css&html for About Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
 
 function Home() {
 	return (
-		<div>
-			Home
+		<div style={{display: 'flex', flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+			<div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '2rem'}}>
+				<div className="home_title_block" style={{}}>
+					<h1 style={{paddingTop: '1rem', color: '#0066B4'}}>Project Name</h1>
+					<h2>Team Name</h2>
+				</div>
+
+				<div className='home_text_block'>
+					<h2 style={{ paddingLeft:'3%'}}>Project Summary</h2>
+					<p className='home_text'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+					</p>
+				</div>
+
+				<div className='home_text_block'>
+					<h2 style={{ paddingLeft:'3%'}}>Current Features</h2>
+					<p className='home_text'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
