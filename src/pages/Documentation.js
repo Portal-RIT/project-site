e547cd6f07ef947f3b77435ffe9d05e967e609d9
eas7048rit <eas7048@g.rit.edu>
2024-03-08 18:08:44 -0500

    switched documentation.js to tab indentations

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -10,12 +10,12 @@ function Documentation() {
 	return (
 		<div style={{"overflow-y":"scroll"}}>
 			<h1>Domain Model</h1>
-            <img src={DOMAIN} alt="Domain Model" height="600px" width="1250px" />
-            <h1>Software Architecture</h1>
-            
-            <a href="https://docs.google.com/document/d/1vfwsHzLObee4xKyIZKxsGICr7fXZqzZAiXjz3rplp4s/edit?usp=sharing"> Architecturally Signifigant Requirements</a>
-            <p/>
-            <img src={ARCHITECTURE} alt="Architecture Diagram" height="750px" width="1250px"/>
+			<img src={DOMAIN} alt="Domain Model" height="600px" width="1250px" />
+			<h1>Software Architecture</h1>
+			
+			<a href="https://docs.google.com/document/d/1vfwsHzLObee4xKyIZKxsGICr7fXZqzZAiXjz3rplp4s/edit?usp=sharing"> Architecturally Signifigant Requirements</a>
+			<p/>
+			<img src={ARCHITECTURE} alt="Architecture Diagram" height="750px" width="1250px"/>
 		</div>
 	)
 }
