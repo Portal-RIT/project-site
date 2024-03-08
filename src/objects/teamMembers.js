8bed5101fe6c668e28cf80963c47c6eaa9c92efa
eas7048rit <eas7048@g.rit.edu>
2024-03-08 17:56:37 -0500

    Completed the team page

---
+++ src/objects/teamMembers.js
@@ -0,0 +1,31 @@
+import mantisShrimp from '../img/mantis_shrimp.png'
+import elephant from '../img/elephant.jpg'
+import possum from '../img/possum.png'
+import ferret from '../img/ferret.jpg'
+
+export const teamMembers = [
+	{
+	    name: "Andrew Chacon",
+	    role: "Developer, Communications Lead",
+	    image: mantisShrimp,
+	    description: "I am a fifth year Software Engineering Major with skills/experience in web development, data processing/preprocessing, web scraping, and analysis and integration of AI/ML models. Current plans are searching for full time positions and getting ready for starting a masters degree in Artificial Intelligence in the Fall."
+	},
+    {
+	    name: "Eric Stricker",
+	    role: "Developer, Scribe",
+	    image: ferret,
+	    description: "I am a fourth year Software Engineering Major with skills and experience focusing on Software Testing (both in creating tests as well as developing frameworks for end-to-end testing), Development on an IDE for use in-house at the company, and Software Architecture Design. In addition to my work experience, I've also worked on video game development and have participated in numerous game jams as well as completed multiple long term personal game development projects."
+	},
+    {
+        name: "Nate Martlew",
+	    role: "Developer",
+	    image: elephant,
+	    description: "I am a fifth year Software Engineering Major with experience in web development, project management, data analytics, and customer relationship management solutions. During my co-ops I have had a focus on web application development using Python and C# to set up APIs that will help in creating dashboard solutions for customers. Current plans are looking for full time positions in the workforce for a Software Engineering role."
+	},
+    {
+        name: "Zoe Bingham",
+        role: "Developer",
+        image: possum,
+        description: "As a fourth-year software engineering student, I've refined my software development, project management, and problem-solving skills. Throughout my co-op experiences, my primary focus has been on web development, gaining skills in front-end frameworks such as Vue, Angular, and React. In addition to these front-end technologies, I've also had experience using express, spring boot, and flask to set up APIs. Beyond work and school, I attend hackathons, work on personal projects, and oversee and drive various projects as the Tech Projects Committee Head of the Computing Organization for Multicultural Students."
+    },
+]
