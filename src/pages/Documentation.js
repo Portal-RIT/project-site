1a5ebe2bf2d790705a122b6f2bb5ae6dd05524b4
Nathan <nm7090@rit.edu>
2024-03-08 17:35:24 -0500

    documentation page

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -1,18 +1,21 @@
 
 import { useLocation } from "react-router-dom";
-import DOMAIN from "../img/Domain_Model.pdf";
-import ARCHITECTURE from "../img/Architecture.pdf";
+import DOMAIN from "../img/Domain_Model.png";
+import ARCHITECTURE from "../img/Architecture.png";
 
 function Documentation() {
 
 	const location = useLocation();
 
 	return (
-		<div>
+		<div style={{"overflow-y":"scroll"}}>
 			<h1>Domain Model</h1>
-            <img src={DOMAIN} alt="Domain Model"/>
+            <img src={DOMAIN} alt="Domain Model" height="600px" width="1250px" />
             <h1>Software Architecture</h1>
-            <img src={ARCHITECTURE} alt="Architecture Diagram"/>
+            
+            <a href="https://docs.google.com/document/d/1vfwsHzLObee4xKyIZKxsGICr7fXZqzZAiXjz3rplp4s/edit?usp=sharing"> Architecturally Signifigant Requirements</a>
+            <p/>
+            <img src={ARCHITECTURE} alt="Architecture Diagram" height="750px" width="1250px"/>
 		</div>
 	)
 }
