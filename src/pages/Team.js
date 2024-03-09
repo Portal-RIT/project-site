05289a56e0dae26888dd2c8d26e61a8cf6cc40c6
Zoe <zmb6893@g.rit.edu>
2024-03-09 12:16:59 -0500

    Made team fit on page and more dynamic

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -3,21 +3,24 @@ import { teamMembers } from '../objects/teamMembers.js';
 
 function Team() {
 	return (
-		<div style={{display: 'flex', flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
-			<div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '1em'}}>
-				{teamMembers.map((teamMember) => (
-					<div className='team_block' style={{textAlign: 'left'}}>
-						<h1 className='team_header'>{teamMember.name} | {teamMember.role}</h1>
-						<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'left'}}>
-							<img src={`${teamMember.image}`} style={{height: '13vh', width: 'auto', marginLeft:'3%', borderRadius: '50%'}}></img>
-							<p className='team_text'>
-								{teamMember.description}
-							</p>
+		<div className='container'>
+			<div style={{display: 'flex', flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+				<div style={{marginLeft: '5%'}}>
+					{teamMembers.map((teamMember) => (
+						<div className='team_block' style={{textAlign: 'left'}}>
+							<h1 className='team_header'>{teamMember.name} | {teamMember.role}</h1>
+							<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'left'}}>
+								<img src={`${teamMember.image}`} style={{height: '13vh', width: 'auto', marginLeft:'3%', borderRadius: '50%'}}></img>
+								<p className='team_text'>
+									{teamMember.description}
+								</p>
+							</div>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</div>
+		
 	)
 }
 
