6923194ab951fe361d460d53e5cacf1ed442cb09
Nathan <nm7090@rit.edu>
2024-04-03 17:52:12 -0400

    added figma iframe

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -29,6 +29,9 @@ function Documentation() {
 			
 			<a href="https://docs.google.com/document/d/1wmaDgJNvd19V3iqBhSzVXEKaSQRj82zdlFYH1MlYwIs/edit?usp=sharing"> Final Project Plan - Google Doc </a>
 
+			<h1>Portal Wireframe</h1>
+			<iframe style={{border: "1px solid rgba(0, 0, 0, 0.1)"}} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FooflxXVx2vUXaBt2a0qg7w%2FPrototype%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DOXvpbGvMy23dshOv-1" allowfullscreen></iframe>
+
 		</div>
 	)
 }
