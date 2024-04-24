44483202c0cd157365942628837af7d40e1baccb
Nathan <nm7090@rit.edu>
2024-04-24 17:49:02 -0400

    added documentation

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -4,7 +4,11 @@ import DOMAIN from "../img/Domain_Model.png";
 import ARCHITECTURE from "../img/Architecture.png";
 import BIG_PICTURE from "../img/Big Picture Plan.png";
 import USER_PERMS from "../img/User Permission UML.png";
-import MESSAGING from "../img/UML - Messaging.png"
+import MESSAGING from "../img/UML - Messaging.png";
+import INTERFACE from "../img/Novena Senior Project - UML - Project Management.png";
+import IMAGES from "../img/Novena Senior Project - UML - Image Processing.png";
+import RELATIONAL from "../img/Novena Senior Project - Relational Entity Diagram.png";
+import INTERFACE_DIAGRAM from "../img/Novena Senior Project - Interface Diagram.png";
 
 
 function Documentation() {
@@ -41,6 +45,28 @@ function Documentation() {
 			<img src={USER_PERMS} alt="User Permissions UML" width="100%" />
 			<h2>Messaging</h2>
 			<img src={MESSAGING} alt="Messaging system UML" width="100%" />
+			<h2>Project Management Interface</h2>
+			<img src={INTERFACE} alt="Project Management Interface" width="100%"/>
+			<h2>Image Processing</h2>
+			<img src={IMAGES} alt="Image Processing" width="100%"/>
+
+			<h1>Relational Database Diagram</h1>
+			<img src={RELATIONAL} alt="Relational Database Diagram" width="100%"/>
+
+			<h1>Interface Diagram</h1>
+			<img src={INTERFACE_DIAGRAM} alt="Interface Diagram" width="100%"/>
+
+			<h1>Tech Stack</h1>
+			<ul style={{"list-style-type":"none"}}>
+				<li>Relational Database (SQL)</li>	
+				<li>Cloud Hosting for Database</li>
+				<li>Document database (Images)</li>
+				<li>Cloud Storage for Documents (Azure or AWS)</li>
+				<li>Frontend: Javascript React / Angular</li>
+				<li>Middleware: Flask, Spring Framework</li>
+				<li>Backend: Python and Java Spring</li>
+				<li>Chromium Web Browsers</li>
+			</ul>			
 
 			<h1>Portal Wireframe</h1>
 			<iframe style={{border: "1px solid rgba(0, 0, 0, 0.1)"}} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FooflxXVx2vUXaBt2a0qg7w%2FPrototype%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DOXvpbGvMy23dshOv-1" allowfullscreen></iframe>
